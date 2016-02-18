db57a8109a525e377e10a5ae1f5dd4c25fcbdf15
Freakils <luvo@projectcodex.co>
2016-02-18 10:13:11 +0200

    working codes

--- app.js
+++ app.js
@@ -3,4 +3,31 @@ var body = document.querySelector("body");
 
 body.onkeydown = function(e){
     //e.keyCode - will capture the key codes
+var keyName = keyCodeName(e.keyCode)
+	displayMessage(keyName);
+
 };
+
+
+
+
+	var keyCodeName = function(keycode) {
+		if(keycode === 37) {
+	 		return "left"; 
+		}
+
+		if(keycode === 38) {
+			return "up";
+		}
+
+		if(keycode === 39) {
+			return "right";
+		}
+
+		if(keycode === 40) {
+			return "down";
+		}
+		
+		else {return ""};
+		
+	};
