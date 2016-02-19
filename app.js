4e75b69f6785e4f7e5f07818a2e9a516c6b5e26d
Freakils <luvo@projectcodex.co>
2016-02-19 16:29:53 +0200

    changes

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+var taxiLocationCounter = 1;
 
 var body = document.querySelector("body");
-
 body.onkeydown = function(e){
     //e.keyCode - will capture the key codes
-var keyName = keyCodeName(e.keyCode)
-	displayMessage(keyName);
+	var keyName = keyCodeName(e.keyCode);
+	
+		if(keyName === "right"){
+		taxiLocationCounter = taxiLocationCounter+1;
+		};
+
+		if(keyName === "left"){
+			taxiLocationCounter = taxiLocationCounter-1;
+		};
+
+
+displayMessage(taxiLocationCounter);
 
 };
+
+	// else if(keyCodeName === "left"){
+	// 	taxiLocationCounter = taxiLocationCounter--;
+	
+// }
 
 
 
@@ -30,4 +45,43 @@ var keyName = keyCodeName(e.keyCode)
 		
 		else {return ""};
 		
+	};
+
+
+	function createLocationClass(number) {
+		if(number ===1) {
+			return "slot-one-of-nine";
+		}
+
+		if(number ===2) {
+			return "slot-two-of-nine";
+		}
+
+		if(number ===3) {
+			return "slot-three-of-nine";
+		}
+
+		if(number ===4) {
+			return "slot-four-of-nine";
+		}
+
+		if(number ===5) {
+			return "slot-five-of-nine";
+		}
+
+		if(number ===6) {
+			return "slot-six-of-nine";
+		}
+
+		if(number ===7) {
+			return "slot-seven-of-nine";
+		}
+
+		if(number ===8) {
+			return "slot-eight-of-nine";
+		}
+
+		if(number ===9) {
+			return "slot-nine-of-nine";
+		}
 	};
